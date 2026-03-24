823dc3848cf721762e17b98030fa04bff2fd561c
蘇 <su@le2.ltd>
2026-03-24 15:29:03 +0800

    npm build

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,17 +3,17 @@ import 'vue-sonner/style.css';
 import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { initializeTheme } from './theme';
 
 const appName = window.document.title;
+const pages = import.meta.glob('./pages/**/*.vue', { eager: true });
 
 initializeTheme();
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
+    resolve: (name) => pages[`./pages/${name}.vue`],
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
