4d446d7fb93650f84837e10403874f98e6ca0479
蘇 <su@le2.ltd>
2026-03-24 09:52:32 +0800

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>{{ config('app.name') }}</title>
+        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
         <script>
             (() => {
                 const root = document.documentElement;
