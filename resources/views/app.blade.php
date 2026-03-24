42bd7ee3fcc113056124edc4c44ea04c0c559542
蘇 <su@le2.ltd>
2026-03-24 15:13:03 +0800

    js 重复导入bug

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,7 +22,7 @@
                 root.classList.toggle('dark', mode === 'dark');
             })();
         </script>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite('resources/js/app.js')
         @inertiaHead
     </head>
     <body class="min-h-screen bg-background font-sans antialiased">
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import Components from "unplugin-vue-components/vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ["resources/js/app.js"],
             refresh: true,
         }),
         vue(),
