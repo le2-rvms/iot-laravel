0914fc259b1e78d70f89ae0ec6ce94a99d7f8ae0
蘇 <su@le2.ltd>
2026-03-26 08:21:02 +0800

    iot_device crud

--- resources/js/components/devices/Filters.vue
+++ resources/js/components/devices/Filters.vue
@@ -30,8 +30,11 @@ function submit() {
 
 <template>
     <div class="rounded-t-[1.5rem] border-b border-app-panel-border/80 px-5 py-4">
-        <form class="flex flex-col gap-3" @submit.prevent="submit">
-            <div class="relative">
+        <form
+            class="grid gap-3 lg:grid-cols-5 xl:grid-cols-[minmax(0,2.4fr)_repeat(4,minmax(0,1fr))_auto] xl:items-center"
+            @submit.prevent="submit"
+        >
+            <div class="relative lg:col-span-5 xl:col-span-1 xl:min-w-0">
                 <Search
                     class="pointer-events-none absolute left-3 top-1/2 size-4 -translate-y-1/2 text-app-subtle-foreground"
                 />
@@ -42,14 +45,12 @@ function submit() {
                 />
             </div>
 
-            <div class="grid gap-3 md:grid-cols-2 xl:grid-cols-4">
-                <UiInput v-model="form.product_key__eq" class="h-11 rounded-xl" placeholder="产品标识" />
-                <UiInput v-model="form.device_status__eq" class="h-11 rounded-xl" placeholder="设备状态" />
-                <UiInput v-model="form.review_status__eq" class="h-11 rounded-xl" placeholder="审核状态" />
-                <UiInput v-model="form.city_relation_id__eq" class="h-11 rounded-xl" placeholder="城市关联ID" />
-            </div>
+            <UiInput v-model="form.product_key__eq" class="h-11 rounded-xl" placeholder="产品标识" />
+            <UiInput v-model="form.device_status__eq" class="h-11 rounded-xl" placeholder="设备状态" />
+            <UiInput v-model="form.review_status__eq" class="h-11 rounded-xl" placeholder="审核状态" />
+            <UiInput v-model="form.city_relation_id__eq" class="h-11 rounded-xl" placeholder="城市关联ID" />
 
-            <div class="flex flex-wrap items-center gap-2">
+            <div class="flex items-center gap-2 lg:justify-end">
                 <UiButton type="submit" class="rounded-xl">搜索</UiButton>
             </div>
         </form>
